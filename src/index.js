85401626f688b20c5d4072289f3bac7521c876fa
giosuranidze <octopusoctopus796@gmail.com>
2022-12-06 14:23:59 +0400

    Created react app and install some deps

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const root = createRoot(document.getElementById("root")); // The ! is used to assume not null state in typescript
+root.render(<App />);
