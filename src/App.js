13afd398455c0619b141da08b13ef47f0fb5c1c0
giosuranidze <octopusoctopus796@gmail.com>
2022-12-06 14:49:49 +0400

    Created routing for basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Box } from "@mui/system"
+import {ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail} from './components/index.js'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+
+  <Router>
+     <Box sx={{backgroundColor:'#000'}}>
+        <Navbar />
+     </Box>
+     <Routes>
+      <Route path="/" exact element={<Feed />} />
+      <Route path="/video/:id" element={<VideoDetail />} />
+      <Route path="/channel/:id" element={<ChannelDetail />} />
+      <Route path="/search/:searchTerm" element={<SearchFeed />} />
+     </Routes>
+  </Router>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import './index.css'
 
-const root = createRoot(document.getElementById("root")); // The ! is used to assume not null state in typescript
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
