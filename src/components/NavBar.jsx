0e31f242f08c7aa66690f904b64890dd45f10ffa
giosuranidze <octopusoctopus796@gmail.com>
2022-12-06 15:24:27 +0400

    implemented navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Stack } from "@mui/system";
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+import { logo } from "../utils/constants";
+import { SearchBar } from "./index";
 
-export default NavBar
+const NavBar = () => (
+  <Stack
+    direction="row"
+    alighItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default NavBar;
